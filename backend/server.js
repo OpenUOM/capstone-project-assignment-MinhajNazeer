255a418b3de27c2beeb8775e7df6b557fdcdaad6
MinhajNazeer <minhajnazeer395@gmail.com>
2023-04-25 10:00:43 +0530

    Update the index.js, server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,7 @@
 const express = require ("express");
-
-const {
-  readTeachers,
-  readStudents,
-  addStudent,
-  addTeacher,
-  deleteTeacher,
-  deleteStudent,
-  readStudentInfo,
-  readTeacherInfo,
-  updateStudent,
-  updateTeacher,
-  dbinitialize
-} = require ("./database.js");
+const bodyParser = require  ("body-parser")
 
 const app = express();
-const bodyParser = require  ("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -31,8 +17,8 @@ app.get("/dbinitialize", async function (req, res) {
 app.get("/listTeachers", async function (req, res) {
   console.log("Request received to list teachers");
   let data = await readTeachers();
-
-  res.setHeader("Content-Type", "application/json");
+  
+  res.setHeader("Content-Type", "application/json");s
   res.end(JSON.stringify(data));
 });
 
@@ -48,20 +34,17 @@ app.post("/getTeacherInfo", async function (req, res) {
 app.post("/addTeacher", async function (req, res) {
   let reqBody = req.body;
   console.log(
-    "Request received to add teacher. Req body: " + JSON.stringify(reqBody)
+  "Request received to add teacher. Req body: " + JSON.stringify(reqBody)
   );
   let data = await addTeacher(reqBody.id, reqBody.name, reqBody.age);
-
+  
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
 });
 
 app.post("/editTeacher", async function (req, res) {
   let reqBody = req.body;
-  console.log(
-    "Request received to update teacher. Req body: " + JSON.stringify(reqBody)
-  );
-  let data = await updateTeacher(reqBody.name,reqBody.age,reqBody.id);
+  let data = await updateTeacher(reqBody.name, reqBody.age, reqBody.id);
 
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
@@ -70,67 +53,10 @@ app.post("/editTeacher", async function (req, res) {
 app.post("/deleteTeacher", async function (req, res) {
   let reqBody = req.body;
   console.log(
-    "Request received to delete teacher. Req body: " + JSON.stringify(reqBody)
+  "Request received to delete teacher. Req body: " + JSON.stringify(reqBody)
   );
   let data = await deleteTeacher(reqBody.id);
-
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data));
-});
-
-// ============== Student Related endpoints ==============
-
-app.get("/listStudents", async function (req, res) {
-  console.log("Request received to list students");
-  let data = await readStudents();
-
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data));
-});
-
-app.post("/getStudentInfo", async function (req, res) {
-  let reqBody = req.body;
-  console.log("Request received to get Student Info");
-  let data = await readStudentInfo(reqBody.id);
-
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data));
-});
-
-app.post("/addStudent", async function (req, res) {
-  let reqBody = req.body;
-  console.log(
-    "Request received to add student. Req body: " + JSON.stringify(reqBody)
-  );
-  let data = await addStudent(
-    reqBody.id,
-    reqBody.name,
-    reqBody.age,
-    reqBody.hometown
-  );
-
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data));
-});
-
-app.post("/deleteStudent", async function (req, res) {
-  let reqBody = req.body;
-  console.log(
-    "Request received to delete student. Req body: " + JSON.stringify(reqBody)
-  );
-  let data = await deleteStudent(reqBody.id);
-
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(data));
-});
-
-app.post("/editStudent", async function (req, res) {
-  let reqBody = req.body;
-  console.log(
-    "Request received to update Student. Req body: " + JSON.stringify(reqBody)
-  );
-  let data = await updateStudent(reqBody.name,reqBody.age,reqBody.hometown,reqBody.id);
-
+  
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
 });
